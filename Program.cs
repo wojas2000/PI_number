7b3b55ab505722669f5e5f102394cc82d5fc3a37
wojas2000 <wojas2000@o2.pl>
2022-03-04 11:56:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PI_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program show number PI with 5 number after point
+            double PI;
+            Console.WriteLine("Program podaje liczbę PI z dokładnością pieciu miejsc po przecinku.");
+            PI = Math.PI;   
+            Console.WriteLine($"Całkowita wartość liczby PI:" + PI);
+            Console.WriteLine("Liczba PI z dokładnością do pięciu miejsc po przecinku: " + PI);
+            Console.WriteLine(Convert.ToString(PI));
+        }
+    }
+}
